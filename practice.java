56082029201cc6c92b3aead28a0c65afa109eb68
Laura Barreto <labarreto@vassar.edu>
2015-02-03 16:43:24 -0500

    edits

--- practice.java
+++ practice.java
@@ -1,11 +1,14 @@
 //Writing a method that iterates through a binary tree and returns the deepest node
 
+//Tree takes in (tree left, node root, tree right);
 
-public Node deepest(Tree here) {
+public Node deepest(Tree t) {
 	//takes in a tree, and returns a node. 
 	//iterate through tree, visit an arbitrary node x, mark node x as visited
+	//goes hand in hand with depth first search. 
 
-	if (here == null) {
+	if (t.here == null) {
 		return here;
 	}
+	//then you want to move to either left or right
 }
