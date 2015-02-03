67c49c6b72eae9ff6e7c36e9f365429aa7061e93
Laura Barreto <labarreto@vassar.edu>
2015-02-03 16:25:38 -0500

    Practicing interview q's
    practice. so the problem is “you have a binary tree and you want to
    return the deepest node in the tree”

---
+++ practice.java
@@ -0,0 +1,11 @@
+//Writing a method that iterates through a binary tree and returns the deepest node
+
+
+public Node deepest(Tree here) {
+	//takes in a tree, and returns a node. 
+	//iterate through tree, visit an arbitrary node x, mark node x as visited
+
+	if (here == null) {
+		return here;
+	}
+}
