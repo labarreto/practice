71f5023214ec46afb60ee1fc308471035f9d1c54
Laura Barreto <labarreto@vassar.edu>
2015-02-17 14:39:42 -0500

    finally figured out how to write a method that will return the deepest node in a tree. FINALLY.

--- practice.java
+++ practice.java
@@ -1,14 +1,30 @@
-//Writing a method that iterates through a binary tree and returns the deepest node
+public void deepNode() {
+            if (isEmpty())
+                return;
+ 
+            Queue q = new Queue();
+            // add root to the queue
+            q.enqueue(root);
 
-//Tree takes in (tree left, node root, tree right);
 
-public Node deepest(Tree t) {
-	//takes in a tree, and returns a node. 
-	//iterate through tree, visit an arbitrary node x, mark node x as  visited
-	//goes hand in hand with depth first search. 
+            // while the queue is not empty
+            while (! q.isEmpty())
+            {   // set current BTNode to equal the item in the queue, while also
+                // removing the item from the queue. 
+                BTNode current = (BTNode) (q.dequeue());
+ 
+                if (current!=null)
+                    // if current is not null, so while it has children, 
+                {
+                    if (current.left!=null)
+                        q.enqueue(current.left);
+                    if (current.right!=null)
+                        q.enqueue(current.right);
+                    if (current.right==null && current.left==null)
+                        System.out.println(current.item);
+                }
+            }
+        }
 
-	if (t.here == null) {
-		return here;
-	}
-	//three types of depth-first traversal: pre-order, in-order, post-order
-}
+// you have a tree t
+// so then you would call t.deepNode() to return deepest node of the tree. 
